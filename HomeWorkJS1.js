1551f1891bdfd6b14a494cf5c3e10b9feeda0ddc
Viktor Shtanko <viktor.v.shtanko@gmail.com>
2021-10-24 16:43:57 +0300

    Complete

---
+++ HomeWorkJS1.js
@@ -0,0 +1,55 @@
+  //1. Write a JS program to compute and print the sum of the two given integers
+
+/*let a=1;
+let b=2;
+ console.log(a+b);*/
+
+    //2. Write a JS program to create a new string adding “Test_" in front of a given string
+
+ /*let a='Eleks Automation Camp 2021'
+    console.log('Test_'+a)*/
+
+    //3. Write a JS program to combine and print your first, last name and age
+
+ /*let a='Viktor';
+ let b='Shtanko';
+ let c=38;
+    console.log('Name-'+a + ','+' Surname-'+b +','+ ' Age-' +c)*/
+
+    //4. Write a JS program to check a given integer is bigger than 100
+
+   /*let a=105;
+        console.log(a>100)*/
+
+    //5. Write a JS program to find if the first number is larger from the two given positive integers
+
+    /*let a=20;
+    let b=10;
+    let c=12;
+        console.log(a>b && a>c);
+    a=5;
+        console.log(a>b && a>c);*/
+
+    //6. Write a JS program to check two given numbers and print true if one of the number is 50 or if their sum is 50
+
+   /* let a=50;
+    let b=0;
+        console.log(a==50 || a+b==50);
+    b=10;
+        console.log(a==50 || a+b==50);
+    a=45;
+    b=5;
+        console.log(a==50 || a+b==50);
+    b=3;
+        console.log(a==50 || a+b==50);*/
+
+    //7. Write a JS program check if a given positive number is a multiple of 3 or a multiple of 7
+
+   /* let a=21;
+    let b=3;
+    let c=7;
+        console.log(a%b==0 || a%c==0);
+    a=30;
+        console.log(a%b==0 || a%c==0);
+    a=17;
+        console.log(a%b==0 || a%c==0);*/
