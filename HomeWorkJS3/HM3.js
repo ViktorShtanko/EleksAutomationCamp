4ff89f693549a6595d565e3dc3fccc4b58ef9277
Viktor Shtanko <viktor.v.shtanko@gmail.com>
2021-11-01 19:07:40 +0200

    Complete

--- HomeWorkJS3/HM3.js
+++ HomeWorkJS3/HM3.js
@@ -1,21 +1,23 @@
 //1. Write a JavaScript function to get the last element of an array
-/*let Alphabet = ['a','b','c'];
+let Alphabet = ['a','b','c'];
 let last = Alphabet.pop();
-    console.log(last);*/
+    console.log(last);
 
 
 //2. Write a simple JavaScript program to join all elements of the following array into a string
 
-/*let Alphabet = ['a','b','c'];
-    console.log(Alphabet.join(";"));*/
+let Alphabet = ['a','b','c'];
+    console.log(Alphabet.join(";"));
 
 //3. There are two arrays with individual values, write a JavaScript program to compute the
 //sum of each individual index value from the given arrays and save them to third array
 
-/*let array1 = [1, 2, 3, 4];
+let array1 = [1, 2, 3, 4];
 let array2 = [5, 6, 7, 8];
-let sum = array1.concat(array2);
-    console.log(sum);*/
+let sum = array1.map(function (num, idx) {
+    return num + array2[idx];
+});
+    console.log(sum);
 
 //4. Write a JavaScript program to calculate the area and perimeter of a rectangle. Rectangle
 //should be an object with properties width and height and methods getArea() and
@@ -24,7 +26,10 @@ let sum = array1.concat(array2);
 let rectangle = {
     Width: 3,
     Height: 4,
-    Area: getArea()  {return rectangle.Width * rectangle.Height;},
+    getArea()  {return this.Width * this.Height;},
     getPerimeter () {return this.Width * 2 + this.Height * 2;}
 };
-console.log ("Area: " + rectangle.Width * rectangle.Height);
+
+
+console.log ("Area: " + rectangle.getArea());
+console.log ("Perimeter: " + rectangle.getPerimeter());
